4e119584445706b0ef1c3c9dc1d6ba6f6bc845dc
Arthur-MONNET <artmon2002@gmail.com>
2023-05-10 15:20:30 +0200

    add stack

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <ul>
+            <li>
+                <nuxt-link to="/about">About</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/contact">Contact</nuxt-link>
+            </li>
+        </ul>
+        <h2>Products</h2>
+        <ul>
+            <li>
+                <nuxt-link to="/products/1">Product 1</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/products/2">Product 2</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/products/3">Product 3</nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
---
+++ pages/products/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        <h1>Product {{id}}</h1>
+        <nuxt-link to="/">Home</nuxt-link>
+    </main>
+</template>
+
+<script setup lang="ts">
+    const route = useRoute()
+    const {id} = route.params
+</script>
